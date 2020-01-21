73b4dff5410ee7b45b86b6850dbd1c4caffc7994
Giacomo Terreni <giacomo.terreni@gmail.com>
2020-01-21 10:49:45 +0100

    composer

---
+++ src/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Gecche\Cupparis\App;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider {
+
+
+    /**
+     * Booting
+     */
+    public function boot()
+    {
+
+    }
+
+	/**
+	 * Register the commands
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+	}
+
+
+}
