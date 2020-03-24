b0febc820282c1d69015560e1baf1acfc9bbe56f
Giacomo Terreni <giacomoterreni@gmail.com>
2020-03-24 11:17:56 +0100

    Models

---
+++ src/Models/AttachmentTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gecche\Cupparis\App\Models;
+
+use Illuminate\Support\Facades\File;
+
+use Carbon\Carbon;
+
+/**
+ * Eloquent model for acl_groups table.
+ * This is used by Eloquent permissions provider.
+ */
+trait AttachmentTrait {
+    
+//    public function getNameExt() {
+//        $ext = $this->ext();
+//        $name = $this->nome;
+//        $name = str_slug($name);
+//        if (ends_with($name, $ext))
+//           return $name;
+//        return $name . $ext;
+//    }
+
+
+    public function getUrl()
+    {
+        return 'downloadmediable/attachment/'.$this->getKey();
+    }
+}
