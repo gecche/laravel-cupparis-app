ff863569de2b4c11e18fa80c95b3d21a92e00f90
Giacomo Terreni <giacomoterreni@gmail.com>
2020-06-01 18:03:56 +0200

    appserviceprovider updated

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Role;
 use App\Models\User;
+use App\Policies\RolePolicy;
 use App\Policies\UserPolicy;
+use Gecche\PolicyBuilder\Facades\PolicyBuilder;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -16,7 +19,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
-        //User::class => UserPolicy::class,
+        User::class => UserPolicy::class,
+        Role::class => RolePolicy::class,
     ];
 
     /**
@@ -32,6 +36,21 @@ class AuthServiceProvider extends ServiceProvider
         // This works in the app by using gate-related functions like auth()->user->can() and @can()
         Gate::before(function ($user, $ability) {
             return $user->hasRole('Superutente') ? true : null;
+        });
+
+        PolicyBuilder::beforeAcl(function ($user, $modelClassName, $context, $builder) {
+
+            if (!$user) {
+                return;
+            }
+
+            //$modelsAllowed = [Comune::class,Area::class,Regione::class,Provincia::class];
+            $modelsAllowed = [];
+            if ($user->hasRole('Superutente') || in_array($modelClassName,$modelsAllowed)) {
+                return PolicyBuilder::all($builder,$modelClassName);
+            }
+
+            return;
         });
     }
 }
