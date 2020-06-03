da3c7530dc1e3ccb7f29aacc07c965a1f0f3bfec
Giacomo Terreni <giacomoterreni@gmail.com>
2020-06-03 13:10:20 +0200

    init aggiornato

--- src/Console/Commands/Init.php
+++ src/Console/Commands/Init.php
@@ -1,7 +1,10 @@
 <?php namespace Gecche\Cupparis\App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\InputStream;
 use Symfony\Component\Process\Process;
 use Illuminate\Support\Str;
 
@@ -14,9 +17,13 @@ class Init extends Command
      * @var string
      */
     protected $signature = 'init 
-                            {--composer : runs shell commands labeled as "composer" (default: no)} 
-                            {--nodb : does not runs shell commands labeled either as "mig" or "seed" (default: yes)} 
-                            {--nomig : does not runs shell commands labeled as "mig" (default: yes)}';
+                            {--force : it forces initialization without prompting (default: no)}
+                            {--stopOnException=0 : stop execution at the first exception (default: no)}
+                            {--type=standard : the initialization type (default: standard)}
+                            {--composer=0 : runs shell commands labeled as "composer" (default: no)} 
+                            {--mig=1 : it runs shell commands labeled either as "mig" or "seed" (default: yes)} 
+                            {--seed=1 : it runs shell commands labeled as "seed" (default: yes)}
+                            {--storage=1 : it runs shell commands labeled as "storage" (default: yes)}';
 
     /**
      * The console command description.
@@ -25,64 +32,176 @@ class Init extends Command
      */
     protected $description = 'Configurazione iniziale del progetto';
 
+
+    protected $storageFoldersToInit = [
+        'allegati',
+        'foto',
+        //'anteprime',
+        'queues',
+        'queues-failed',
+        'temp',
+        'datafile',
+    ];
+
+    protected $optionsToCheckForExecution = [
+      'composer',
+      'mig',
+      'seed',
+      'storage',
+    ];
+
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle()
+    protected function handleAfterConfirmation()
     {
 
-        $cmdArray = [
-            env('COMPOSER_PATH','composer').' update' => 'composer',
-            env('COMPOSER_PATH','composer').' dump-autoload' => 'composer',
-            'php artisan migrate:reset' => 'mig',
-            'php artisan migrate' => 'mig',
-            'php artisan db:seed' => 'seed',
-        ];
+        $cmdsArray = $this->prepareCommandsArray();
 
-        $phpPath = env('PHP_PATH', 'php');
         $path = base_path();
 
         $cmdArrayProcessed = [];
-        foreach ($cmdArray as $cmd => $group) {
-            if (!$this->option('composer') && in_array($group, ['composer'])) {
-                continue;
+        foreach ($cmdsArray as $cmdArray) {
+
+            foreach ($cmdArray as $group => $cmd) {
+
+                if (!$this->checkExecutionFromOptions($group, $cmd)) {
+                    continue;
+                };
+
+                $cmdArrayProcessed[] = implode(' ', $cmd)."\n";
+
+                $inputFile = null;
+                $input = null;
+                if (array_key_exists('input',$cmd)) {
+                    $inputFile = $cmd['input'];
+                    unset($cmd['input']);
+                }
+
+                //SE SERVONO FEATURES DI SHELL (NCHE SOLO *) BISOGNA PASSARE OLD SCHOOL LA STRINGA DEL COMANDO
+                if (array_key_exists('shell',$cmd)) {
+                    $process = Process::fromShellCommandline($cmd['shell'], $path);
+                    unset($cmd['shell']);
+                } else {
+
+                    $process = new Process($cmd, $path);
+                }
+
+                $process->setTimeout(null);
+                if (File::exists($inputFile)) {
+                    $input = new InputStream();
+                    $stream = File::get($inputFile);
+//                    print_r($stream);
+                    $input->write($stream);
+                    $process->setInput($input);
+                }
+                $process->start();
+
+                if (!is_null($input)) {
+                    $input->close();
+                }
+
+                $process->wait(function ($type, $buffer) {
+                    if (Process::ERR === $type) {
+                        echo 'ERR > '.$buffer;
+                    }
+//                    else {
+//                        echo 'OUT > '.$buffer;
+//                    }
+                });
+
+
+                // executes after the command finishes
+                if (!$process->isSuccessful()) {
+
+                    $e = new ProcessFailedException($process);
+                    if ($this->option('stopOnException')) {
+                        throw $e;
+                    } else {
+                        $this->comment($e->getMessage());
+                    }
+
+                }
+
+
+                echo $process->getOutput();
             }
-
-            if ($this->option('nodb') && in_array($group, ['mig', 'seed'])) {
-                continue;
-            }
-            if ($this->option('nomig') && in_array($group, ['mig'])) {
-                continue;
-            }
-
-            if (Str::startsWith($cmd, 'php')) {
-                $cmd = $phpPath . substr($cmd, 3);
-            }
-
-            $cmdArrayProcessed[] = $cmd;
-
-            $process = new Process($cmd, $path);
-            $process->setTimeout(null);
-            $process->run();
-
-// executes after the command finishes
-            if (!$process->isSuccessful()) {
-                throw new ProcessFailedException($process);
-            }
-
-            echo $process->getOutput();
         }
 
-        $finalCommand = implode(';', $cmdArrayProcessed);
+        $finalCommand = implode(' ', $cmdArrayProcessed);
 
 
-        echo $finalCommand . "\n";
+        $this->comment('Comandi eseguiti: ');
+
+        $this->comment($finalCommand);
+
         $this->comment('Inizializzazione completata');
 
 
     }
 
 
+    protected function checkExecutionFromOptions($group,$cmd) {
+
+        foreach ($this->optionsToCheckForExecution as $optionToCheck) {
+            if (!$this->option($optionToCheck) && in_array($group, [$optionToCheck])) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    protected function prepareCommandsArray() {
+
+        switch ($this->option('type')) {
+            case 'standard':
+
+                $cmdArray = [
+                    ['composer' => [env('COMPOSER_PATH','composer'),'update']],
+                    ['composer' => [env('COMPOSER_PATH','composer'),'dump-autoload']],
+                ];
+
+                foreach ($this->storageFoldersToInit as $folder) {
+                    $cmdArray[] = ['storage' => ['shell' => 'rm -rf '. storage_path() . '/files/'.$folder.'/*']];
+                }
+
+                $postStorageCmdArray = [
+                    ['mig' => [env('PHP_PATH','php'),'artisan','migrate:reset']],
+                    ['mig' => [env('PHP_PATH','php'),'artisan','migrate']],
+                    ['seed' => [env('PHP_PATH','php'),'artisan','db:seed']],
+                    ['seed' => [env('PHP_PATH','php'),'artisan','permissions']],
+                ];
+
+                $cmdArray = array_merge($cmdArray,$postStorageCmdArray);
+
+            break;
+            default:
+                $cmdArray = [];
+            break;
+        }
+        return $cmdArray;
+
+    }
+
+
+    public function handle() {
+        if ($this->option('force')) {
+            return $this->handleAfterConfirmation();
+        }
+
+        $this->comment("\nDominio ". app()->domain());
+        $confirmMessage1 = "Continuare inizializzazione ?";
+
+        if ($this->confirm($confirmMessage1)) {
+            $this->comment("DATABASE ". env('DB_DATABASE','NO DB'));
+            $this->comment("\nCARTELLA STORAGE ". storage_path());
+            $confirmMessage2 = "Il comando inizializzerà il database e la cartella di storage indicate, continuo?";
+            if ($this->confirm($confirmMessage2)) {
+                return $this->handleAfterConfirmation();
+            }
+        }
+    }
 }
