8c820f3ddb9785b8533f8ae1d2915592824de75c
Giacomo Terreni <giacomoterreni@gmail.com>
2020-06-08 15:28:46 +0200

    autocomplete search

--- src/Foorm/Base/Actions/Autocomplete.php
+++ src/Foorm/Base/Actions/Autocomplete.php
@@ -4,9 +4,17 @@ namespace Gecche\Cupparis\App\Foorm\Base\Actions;
 
 
 use Gecche\Foorm\Actions\Autocomplete as BaseAutocomplete;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class Autocomplete extends BaseAutocomplete
 {
 
+    protected function init()
+    {
+        $field = Arr::get($this->input, 'field');
+        $this->fieldToAutocomplete = Str::startsWith($field,'s_') ? substr($field,2) : $field;
+        $this->value = Arr::get($this->input, 'value');
+    }
 
 }
