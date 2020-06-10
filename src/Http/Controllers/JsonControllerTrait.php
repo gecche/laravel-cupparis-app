52db159c049d92f3440ac9ad21f73f3169b2afa5
Giacomo Terreni <giacomoterreni@gmail.com>
2020-06-10 16:06:11 +0200

    JsonControllerTrait

---
+++ src/Http/Controllers/JsonControllerTrait.php
@@ -0,0 +1,28 @@
+<?php namespace Gecche\Cupparis\App\Http\Controllers;
+
+use Illuminate\Support\Facades\Response;
+
+trait JsonControllerTrait
+{
+
+    protected $json = [
+        'error' => 0,
+        'msg' => '',
+    ];
+
+
+    protected function _error($msg)
+    {
+        $this->json['error'] = 1;
+        $this->json['msg'] = $msg;
+    }
+
+    protected function _json($msg = false)
+    {
+        if ($msg !== false) {
+            $this->json['msg'] = $msg;
+        }
+        return Response::json($this->json);
+    }
+
+}
