9892a92b836ef0be1e6a8fdebbab7480b6a979d7
Giacomo Terreni <giacomo.terreni@gmail.com>
2020-06-21 00:57:18 +0200

    appvar

--- app/Models/AppVar.php
+++ app/Models/AppVar.php
@@ -7,25 +7,9 @@ class AppVar extends Breeze {
     //public $timestamps = false;
 
     protected $table = 'vars';
-    
-    protected $fillable = array('id', 'value');
-    
-    public static $relationsData = array(
-        //'address' => array(self::HAS_ONE, 'Address'),
-        //'orders'  => array(self::HAS_MANY, 'Order'),
-        //'user' => array(self::BELONGS_TO, 'User'),
-    );
 
-    public static function initializeVar($id,$value = 0) {
-        $appvar = static::findOrNew(['id'=>$id]);
-        if ($appvar->value) {
-            return $appvar;
-        }
-        $appvar->value = 0;
-        $appvar->save();
-        return $appvar;
-    }
+    protected $fillable = ['id', 'value'];
 
-
+    public static $relationsData = [];
 
 }
---
+++ app/Services/AppVar.php
@@ -0,0 +1,7 @@
+<?php namespace App\Services;
+
+class AppVar extends \Gecche\Cupparis\App\Services\AppVar
+{
+
+
+}
