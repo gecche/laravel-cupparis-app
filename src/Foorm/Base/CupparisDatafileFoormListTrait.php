302995eae4eb543cd2920c6e6cc2e97e337cc202
gecche <giacomo.terreni@gmail.com>
2021-02-22 11:07:46 +0100

    Datafile Form List con sheets

--- src/Foorm/Base/CupparisDatafileFoormListTrait.php
+++ src/Foorm/Base/CupparisDatafileFoormListTrait.php
@@ -2,6 +2,7 @@
 
 namespace Gecche\Cupparis\App\Foorm\Base;
 
+use App\Models\Datafile;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
@@ -10,22 +11,26 @@ trait CupparisDatafileFoormListTrait
 
     protected $displayOnlyErrors = false;
 
+    protected $datafileId;
+
     protected function init()
     {
         parent::init();
         $this->checkDisplayOnlyErrorsFromInput();
+        $this->setDatafileId();
         return;
     }
 
-    protected function checkDisplayOnlyErrorsFromInput() {
-        if (Arr::get($this->input,'datafile_only_errors')) {
+    protected function checkDisplayOnlyErrorsFromInput()
+    {
+        if (Arr::get($this->input, 'datafile_only_errors')) {
             $this->displayOnlyErrors = true;
         }
     }
 
     protected function applyListBuilder()
     {
-        if (Arr::get($this->customFuncs,'listBuilder') instanceof \Closure) {
+        if (Arr::get($this->customFuncs, 'listBuilder') instanceof \Closure) {
             $builder = $this->customFuncs['listBuilder'];
             $this->formBuilder = $builder($this->model);
             return;
@@ -35,36 +40,44 @@ trait CupparisDatafileFoormListTrait
         $this->formBuilder = $modelClass::acl();
 
         if ($this->displayOnlyErrors) {
-            $this->paginateSelect = array($this->model->getTable().".*");
-            $this->model->setDefaultOrderColumns(["datafile_sheet" => 'ASC',"row" => "ASC"]);
-            $this->formBuilder = $this->formBuilder->join('datafile_error',"datafile_error.datafile_table_id", "=",$this->model->getTable().".id")
-                ->where('datafile_error.datafile_table_type',$modelClass)
+            $this->paginateSelect = array($this->model->getTable() . ".*");
+            $this->model->setDefaultOrderColumns(["datafile_sheet" => 'ASC', "row" => "ASC"]);
+            $this->formBuilder = $this->formBuilder->join('datafile_error', "datafile_error.datafile_table_id", "=",
+                $this->model->getTable() . ".id")
+                ->where('datafile_error.datafile_table_type', $modelClass)
                 ->groupBy("datafile_error.datafile_table_id");
 
         }
 
     }
 
-    public function setFormMetadata() {
+    public function setFormMetadata()
+    {
         parent::setFormMetadata();
 
         $this->setFormMetadataHasErrors();
+        $this->setFormMetadataSheets();
     }
 
-    protected function setFormMetadataHasErrors() {
+    protected function setDatafileId()
+    {
         $fixedConstraints = Arr::get($this->params, 'fixed_constraints', []);
-        $datafileId = null;
+        $this->datafileId = null;
         foreach ($fixedConstraints as $fixedConstraint) {
             if (Arr::get($fixedConstraint, 'field') == 'datafile_id') {
-                $datafileId = Arr::get($fixedConstraint, 'value');
+                $this->datafileId = Arr::get($fixedConstraint, 'value');
+                return;
             }
         }
+    }
 
+    protected function setFormMetadataHasErrors()
+    {
         $hasErrors = 0;
-        if ($datafileId) {
+        if ($this->datafileId) {
             $hasErrors = DB::table('datafile_error')
                 ->select(['datafile_table_id'])
-                ->where("datafile_id", $datafileId)
+                ->where("datafile_id", $this->datafileId)
                 ->where('datafile_table_type', get_class($this->model))
                 ->groupBy('datafile_table_id')
                 ->get();
@@ -76,5 +89,26 @@ trait CupparisDatafileFoormListTrait
         $this->formMetadata['has_datafile_errors'] = $hasErrors;
     }
 
+    protected function setFormMetadataSheets()
+    {
+        $anyOption = [
+            $this->config['null-value'] => $this->config['any-label'],
+        ];
+
+
+        $sheets = [
+
+        ];
+        if ($this->datafileId) {
+            $datafile = Datafile::find($this->datafileId);
+            if ($datafile) {
+                $sheets = $datafile->datafile_sheet;
+            }
+        }
+
+        $sheets = $anyOption + $sheets;
+        $this->formMetadata['sheets'] = $sheets;
+        $this->formMetadata['sheets_order'] = array_keys($sheets);
+    }
 
 }
