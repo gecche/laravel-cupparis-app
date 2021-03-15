3ddb96691e052f778160265eea0de7af6653d92c
vagrant <vagrant@homestead>
2021-03-15 17:29:07 +0000

    modificato home

--- resources/views/crud-1.1/bootstrap4/home.blade.php
+++ resources/views/crud-1.1/bootstrap4/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">GAP</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in! <a href="/dashboard">Dashboard</a>
+                    Amministrazione
+{{--                    You are logged in! <a href="/dashboard">Dashboard</a>--}}
                 </div>
             </div>
         </div>
