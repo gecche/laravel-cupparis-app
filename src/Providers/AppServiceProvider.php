ce6b8c740476c088124a620f80eea6901e8f8c7a
Giacomo Terreni <giacomoterreni@gmail.com>
2022-02-11 18:19:10 +0100

    bugfix

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -21,80 +21,80 @@ class AppServiceProvider extends ServiceProvider
     {
         $rootDir = __DIR__ . '/../../';
 
-        $crudVersion = env('CRUD_VERSION','1.0');
+        $crudVersion = env('CRUD_VERSION', '1.0');
 
         //Publishing configs
         $this->publishes([
-            $rootDir. 'config/cupparis-app.php' => config_path('cupparis-app.php'),
+            $rootDir . 'config/cupparis-app.php' => config_path('cupparis-app.php'),
         ], 'public');
 
         if (!is_dir(base_path('cupparis'))) {
             mkdir(base_path('cupparis'));
         }
         $this->publishes([
-            $rootDir. 'cupparis-app.json' => base_path('cupparis-app.json'),
+            $rootDir . 'cupparis-app.json' => base_path('cupparis-app.json'),
         ], 'public');
 
         if (!is_dir(config_path('foorms'))) {
             mkdir(config_path('foorms'));
         }
         $this->publishes([
-            $rootDir. 'config-packages/auth.php' => config_path('auth.php'),
-            $rootDir. 'config-packages/breeze.php' => config_path('breeze.php'),
-            $rootDir. 'config-packages/filesystems.php' => config_path('filesystems.php'),
-            $rootDir. 'config-packages/foorm.php' => config_path('foorm.php'),
-            $rootDir. 'config-packages/image.php' => config_path('image.php'),
-            $rootDir. 'config-packages/imagecache.php' => config_path('imagecache.php'),
-            $rootDir. 'config-packages/permission.php' => config_path('permission.php'),
-            $rootDir. 'config-packages/themes.php' => config_path('themes.php'),
-            $rootDir. 'config-packages/foorms/user.php' => config_path('foorms/user.php'),
+            $rootDir . 'config-packages/auth.php' => config_path('auth.php'),
+            $rootDir . 'config-packages/breeze.php' => config_path('breeze.php'),
+            $rootDir . 'config-packages/filesystems.php' => config_path('filesystems.php'),
+            $rootDir . 'config-packages/foorm.php' => config_path('foorm.php'),
+            $rootDir . 'config-packages/image.php' => config_path('image.php'),
+            $rootDir . 'config-packages/imagecache.php' => config_path('imagecache.php'),
+            $rootDir . 'config-packages/permission.php' => config_path('permission.php'),
+            $rootDir . 'config-packages/themes.php' => config_path('themes.php'),
+            $rootDir . 'config-packages/foorms/user.php' => config_path('foorms/user.php'),
         ], 'public');
 
 
         //Publishing and overwriting app folders
         $this->publishes([
-            $rootDir. 'bootstrap/app.php' => base_path('bootstrap/app.php'),
-            $rootDir. 'app/Console/Kernel.php' => app_path('Console/Kernel.php'),
-            $rootDir. 'app/Console/Commands' => app_path('Console/Commands'),
-            $rootDir. 'app/Foorm' => app_path('Foorm'),
-            $rootDir. 'app/Models' => app_path('Models'),
-            $rootDir. 'app/Policies' => app_path('Policies'),
-            $rootDir. 'app/Services' => app_path('Services'),
-            $rootDir. 'app/Providers/AppServiceProvider.php' => app_path('Providers/AppServiceProvider.php'),
-            $rootDir. 'app/Providers/AuthServiceProvider.php' => app_path('Providers/AuthServiceProvider.php'),
-            $rootDir. 'app/Providers/EventServiceProvider.php' => app_path('Providers/EventServiceProvider.php'),
-            $rootDir. 'app/Http/Kernel.php' => app_path('Http/Kernel.php'),
-            $rootDir. 'app/Http/Controllers/Controller.php' => app_path('Http/Controllers/Controller.php'),
-            $rootDir. 'app/Http/Controllers/DownloadController.php' => app_path('Http/Controllers/DownloadController.php'),
-            $rootDir. 'app/Http/Controllers/FoormActionController.php' => app_path('Http/Controllers/FoormActionController.php'),
-            $rootDir. 'app/Http/Controllers/FoormController.php' => app_path('Http/Controllers/FoormController.php'),
-            $rootDir. 'app/Http/Controllers/HomeController.php' => app_path('Http/Controllers/HomeController.php'),
-            $rootDir. 'app/Http/Controllers/MiscController.php' => app_path('Http/Controllers/MiscController.php'),
-            $rootDir. 'app/Http/Controllers/ModelSkeletonController.php' => app_path('Http/Controllers/ModelSkeletonController.php'),
-            $rootDir. 'app/Http/Controllers/TestController.php' => app_path('Http/Controllers/TestController.php'),
-            $rootDir. 'app/Http/Kernel.php' => app_path('Http/Kernel.php'),
+            $rootDir . 'bootstrap/app.php' => base_path('bootstrap/app.php'),
+            $rootDir . 'app/Console/Kernel.php' => app_path('Console/Kernel.php'),
+            $rootDir . 'app/Console/Commands' => app_path('Console/Commands'),
+            $rootDir . 'app/Foorm' => app_path('Foorm'),
+            $rootDir . 'app/Models' => app_path('Models'),
+            $rootDir . 'app/Policies' => app_path('Policies'),
+            $rootDir . 'app/Services' => app_path('Services'),
+            $rootDir . 'app/Providers/AppServiceProvider.php' => app_path('Providers/AppServiceProvider.php'),
+            $rootDir . 'app/Providers/AuthServiceProvider.php' => app_path('Providers/AuthServiceProvider.php'),
+            $rootDir . 'app/Providers/EventServiceProvider.php' => app_path('Providers/EventServiceProvider.php'),
+            $rootDir . 'app/Http/Kernel.php' => app_path('Http/Kernel.php'),
+            $rootDir . 'app/Http/Controllers/Controller.php' => app_path('Http/Controllers/Controller.php'),
+            $rootDir . 'app/Http/Controllers/DownloadController.php' => app_path('Http/Controllers/DownloadController.php'),
+            $rootDir . 'app/Http/Controllers/FoormActionController.php' => app_path('Http/Controllers/FoormActionController.php'),
+            $rootDir . 'app/Http/Controllers/FoormController.php' => app_path('Http/Controllers/FoormController.php'),
+            $rootDir . 'app/Http/Controllers/HomeController.php' => app_path('Http/Controllers/HomeController.php'),
+            $rootDir . 'app/Http/Controllers/MiscController.php' => app_path('Http/Controllers/MiscController.php'),
+            $rootDir . 'app/Http/Controllers/ModelSkeletonController.php' => app_path('Http/Controllers/ModelSkeletonController.php'),
+            $rootDir . 'app/Http/Controllers/TestController.php' => app_path('Http/Controllers/TestController.php'),
+            $rootDir . 'app/Http/Kernel.php' => app_path('Http/Kernel.php'),
         ], 'public');
 
         //Publishing and overwriting databases folders
         $this->publishes([
-            $rootDir. 'database/factories' => database_path('factories'),
-            $rootDir. 'database/migrations' => database_path('migrations'),
-            $rootDir. 'database/seeds' => database_path('seeds'),
+            $rootDir . 'database/factories' => database_path('factories'),
+            $rootDir . 'database/migrations' => database_path('migrations'),
+            $rootDir . 'database/seeds' => database_path('seeds'),
         ], 'public');
 
         //Publishing and overwriting resources folders
         $this->publishes([
-            $rootDir. 'resources/documenti' => base_path('resources/documenti'),
-            $rootDir. 'resources/lang' => base_path('resources/lang'),
+            $rootDir . 'resources/documenti' => base_path('resources/documenti'),
+            $rootDir . 'resources/lang' => base_path('resources/lang'),
         ], 'public');
 
         //Publishing and overwriting public folders
         $this->publishes([
-            $rootDir. 'public/bootstrap4' => public_path('bootstrap4'),
-            $rootDir. 'public/images' => public_path('images'),
-            $rootDir. 'public/bootstrap4-app' => public_path('bootstrap4-app'),
-            $rootDir. 'public/smarty3-app' => public_path('smarty3-app'),
-            $rootDir. 'public/admin' => public_path('admin'),
+            $rootDir . 'public/bootstrap4' => public_path('bootstrap4'),
+            $rootDir . 'public/images' => public_path('images'),
+            $rootDir . 'public/bootstrap4-app' => public_path('bootstrap4-app'),
+            $rootDir . 'public/smarty3-app' => public_path('smarty3-app'),
+            $rootDir . 'public/admin' => public_path('admin'),
             //$rootDir. 'public/js/edit_area' => public_path('js/edit_area'),
             //$rootDir. 'public/crud-vue/components' => public_path('crud-vue/components'),
             //$rootDir. 'public/crud-vue/ModelConfs' => public_path('crud-vue/ModelConfs'),
@@ -102,23 +102,20 @@ class AppServiceProvider extends ServiceProvider
         ], 'public');
 
 
-
-
-        if (env('CRUD_ENV','') != 'develop') {
+        if (env('CRUD_ENV', '') != 'develop') {
             $this->publishes([
-                __DIR__ . "/../public/crud-$crudVersion/bootstrap4" => public_path('bootstrap4'),
-                __DIR__ . "/../public/crud-$crudVersion/smarty3" => public_path('smarty3'),
-                __DIR__ . "/../resources/views/crud-$crudVersion/bootstrap4" => resource_path('views/bootstrap4'),
-                __DIR__ . "/../resources/views/crud-$crudVersion/smarty3" => resource_path('views/smarty3'),
-                $rootDir. 'resources/views/bootstrap4-app' => resource_path('views/bootstrap4-app'),
-                $rootDir. 'resources/views/smarty3-app' => resource_path('views/smarty3-app'),
-                $rootDir. 'resources/views/admin' => resource_path('views/admin'),
+                $rootDir . "public/crud-$crudVersion/bootstrap4" => public_path('bootstrap4'),
+                $rootDir . "public/crud-$crudVersion/smarty3" => public_path('smarty3'),
+                $rootDir . "resources/views/crud-$crudVersion/bootstrap4" => resource_path('views/bootstrap4'),
+                $rootDir . "resources/views/crud-$crudVersion/smarty3" => resource_path('views/smarty3'),
+                $rootDir . 'resources/views/bootstrap4-app' => resource_path('views/bootstrap4-app'),
+                $rootDir . 'resources/views/smarty3-app' => resource_path('views/smarty3-app'),
+                $rootDir . 'resources/views/admin' => resource_path('views/admin'),
             ], 'templates');
         }
 
 
-
-        $this->loadRoutesFrom($rootDir . 'routes/web.php');
+        $this->loadRoutesFrom($rootDir . 'src/routes/web.php');
 
         $this->bootBlade();
 
@@ -177,9 +174,10 @@ class AppServiceProvider extends ServiceProvider
         Validator::extend('codice_fiscale_professional', 'Gecche\Cupparis\App\Validation\Rules@codiceFiscaleProfessional');
     }
 
-    protected function bootFilesystemMacros() {
-        Filesystem::macro('deleteFiles', function ($pattern,$flags = 0) {
-            File::delete(File::glob($pattern,$flags));
+    protected function bootFilesystemMacros()
+    {
+        Filesystem::macro('deleteFiles', function ($pattern, $flags = 0) {
+            File::delete(File::glob($pattern, $flags));
         });
 
         Filesystem::macro('mimeFromGuesser', function ($path) {
