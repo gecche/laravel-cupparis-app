bd6a66ba3641144978d0b17d4ec5d947b1333a27
Giacomo Terreni <giacomoterreni@gmail.com>
2023-01-26 09:28:15 +0100

    wake up

--- src/Services/DownloadService.php
+++ src/Services/DownloadService.php
@@ -46,27 +46,6 @@ class DownloadService {
 
     }
 
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * *Singleton* instance.
-     *
-     * @return void
-     */
-    private function __clone()
-    {
-    }
-
-    /**
-     * Private unserialize method to prevent unserializing of the *Singleton*
-     * instance.
-     *
-     * @return void
-     */
-    private function __wakeup()
-    {
-    }
-
-
     public function getUploadValidationRules($type) {
 
 
