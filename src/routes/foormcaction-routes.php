1ce37596c4b403c75b1c8533f63ed0256628e555
Giacomo Terreni <giacomo.terreni@gmail.com>
2026-04-24 16:22:51 +0200

    bugfix routes

--- src/routes/foormcaction-routes.php
+++ src/routes/foormcaction-routes.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\FoormActionController;
 
     Route::post('queue/{foormaction}/{foorm}/{foormtype}/{constraintField}/{constraintValue}/{foormpk?}', [\App\Http\Controllers\FoormActionQueueController::class,'foormCAction'])->where([
         'foorm' => $whereFoorm,
-        'foormaction' => $whereFoormAction
+        'foormaction' => $whereFoormCAction
     ]);
     Route::post('{foormcaction}/{foorm}/{foormtype}/{constraintField}/{constraintValue}/{$foormPk?}', [FoormActionController::class, 'foormCAction'])->where([
         'foorm' => $whereFoorm,
